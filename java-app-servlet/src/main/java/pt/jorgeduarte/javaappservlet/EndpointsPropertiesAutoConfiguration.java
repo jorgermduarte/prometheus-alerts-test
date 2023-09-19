ac180182be07325ecac0dfba0537dd08be83886e
Joka <joka867@hotmail.com>
2023-09-19 13:38:10 +0100

    feat: removed unused code, removed dependencies

--- java-app-servlet/src/main/java/pt/jorgeduarte/javaappservlet/EndpointsPropertiesAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package pt.jorgeduarte.javaappservlet;
-
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.PropertySource;
-
-@AutoConfiguration
-@PropertySource("classpath:endpoints.properties")
-public class EndpointsPropertiesAutoConfiguration {
-}
--- java-app-servlet/src/main/java/pt/jorgeduarte/javaappservlet/ServletExample/CustomServlet.java
+++ java-app-servlet/src/main/java/pt/jorgeduarte/javaappservlet/ServletExample/CustomServlet.java
@@ -1,21 +1,28 @@
 package pt.jorgeduarte.javaappservlet.ServletExample;
 
+import io.prometheus.client.hotspot.DefaultExports;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "CustomServlet", urlPatterns = "/myservlet")
 public class CustomServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        DefaultExports.initialize();
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>Olá, Servlet!</h1>");
-        out.println("</body></html>");
+
+        response.getWriter().println("<html><body>");
+        response.getWriter().println("<h1>Olá, Servlet!</h1>");
+        response.getWriter().println("</body></html>");
     }
 }
